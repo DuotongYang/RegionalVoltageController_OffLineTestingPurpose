73e14a331b3952bdc9dd4f71e0388f398ba0fef0
duotongyang <duotong1@vt.edu>
2017-03-26 14:14:16 -0400

    RegionalVoltageController Adapter

---
+++ RegionalVoltageControl/RegionalVoltageControl/RegionalVoltageControllerAdapter.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+using System.Collections;
+using SynchrophasorAnalytics.Modeling;
+
+
+namespace RegionalVoltageControl
+{
+	[Serializable()]
+	public class RegionalVoltageControllerAdapter
+	{
+		#region [ Private Members ]
+		private NetworkModel m_networkModel;
+		#endregion
+
+
+		#region [ Properties ]
+		[XmlElement("Model")]
+		public NetworkModel Model
+		{
+			get
+			{
+				return m_networkModel;
+			}
+			set
+			{
+				m_networkModel = value;
+			}
+
+		}
+
+
+		#endregion
+	}
+
+	class Program
+	{
+		public static void Main()
+		{
+
+		}
+		
+
+
+	}
+
+
+}
